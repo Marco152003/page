f7d80c90a723ec26a97aefa5bff33ab35012b13c
susana <marcoalejandrotafurari@gmail.com>
2023-02-27 07:45:10 -0500

    Inicio del sitio web

---
+++ url/work/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def tarea1(request,*cadena,**cadenaID):
+    
+    
+
+
+
+    return render(request,'trabajo1.html')
